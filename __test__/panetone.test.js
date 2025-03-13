092de3a83ba728f4151ebcd844b8a690888a2541
MANUELA CRISTINA TORRES GUIMARÃES <manuela_c_guimaraes@estudante.sesisenai.org.br>
2025-03-13 08:51:57 -0300

    test pronto

--- __test__/panetone.test.js
+++ __test__/panetone.test.js
@@ -19,14 +19,14 @@ describe("Teste unitário da classe panetone", ()=>{
 
         expect(panetone.calcularMassaEspecifica()).toBeCloseTo(massaEspecifica, 2)
     })
-    test("Calculo do ingredientes do panetone", ()=>{
+    test("Calculo do ingredientes da massa de 700 do panetone", ()=>{
 
         const massa = 700
 
         const farinha = (250 * massa) / 440
         const fermento = (10 * massa) / 440
-        const leite = 
-        const ovos = (2 * massa) / 440
+        const leite = (60 * massa) / 440
+        const ovos = arredondar((2 * massa) / 440)
         const manteiga = (55 * massa) / 440
         const acucar = (60 * massa) / 440
         const mel = (10 * massa) / 440
@@ -34,6 +34,54 @@ describe("Teste unitário da classe panetone", ()=>{
         const frutas = (100 * massa) / 440
         const raspas = (5 * massa) / 440
         const sal = (2 * massa) / 440
+
+        const ingredientes = new Panetone(farinha, fermento, leite, ovos, manteiga, acucar, mel, baunilha, frutas, raspas, sal)
+
+        expect(ingredientes.calcularIngredientes()).toBeCloseTo(2)
+
+    })
+
+    test("Calculo do ingredientes da massa de 440 do panetone", ()=>{
+
+        const massa = 440
+
+        const farinha = (250 * massa) / 440
+        const fermento = (10 * massa) / 440
+        const leite = (60 * massa) / 440
+        const ovos = arredondar((2 * massa) / 440)
+        const manteiga = (55 * massa) / 440
+        const acucar = (60 * massa) / 440
+        const mel = (10 * massa) / 440
+        const baunilha = (5 * massa) / 440
+        const frutas = (100 * massa) / 440
+        const raspas = (5 * massa) / 440
+        const sal = (2 * massa) / 440
+
+        const ingredientes = new Panetone(farinha, fermento, leite, ovos, manteiga, acucar, mel, baunilha, frutas, raspas, sal)
+
+        expect(ingredientes.calcularIngredientes()).toBeCloseTo(2)
+
+    })
+
+    test("Calculo do ingredientes da massa de 180 do panetone", ()=>{
+
+        const massa = 180
+
+        const farinha = (250 * massa) / 440
+        const fermento = (10 * massa) / 440
+        const leite = (60 * massa) / 440
+        const ovos = arredondar((2 * massa) / 440)
+        const manteiga = (55 * massa) / 440
+        const acucar = (60 * massa) / 440
+        const mel = (10 * massa) / 440
+        const baunilha = (5 * massa) / 440
+        const frutas = (100 * massa) / 440
+        const raspas = (5 * massa) / 440
+        const sal = (2 * massa) / 440
+
+        const ingredientes = new Panetone(farinha, fermento, leite, ovos, manteiga, acucar, mel, baunilha, frutas, raspas, sal)
+
+        expect(ingredientes.calcularIngredientes()).toBeCloseTo(2)
 
     })
 })
